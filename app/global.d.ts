7d416f5252c72d6028c519095683436117a13e2e
Josh Lindsay <182923+jlindsayDev@users.noreply.github.com>
2025-05-20 09:59:08 -0700

    add module for model types

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,12 +1,35 @@
-// app/global.d.ts
-import type {} from 'hono'
-
 type Head = {
-  title?: string
+    title?: string;
+};
+
+declare module "hono" {
+    type ContextRenderer = (
+        content: string | Promise<string>,
+        head?: Head,
+    ) => Response | Promise<Response>;
 }
 
-declare module 'hono' {
-  interface ContextRenderer {
-    (content: string | Promise<string>, head?: Head): Response | Promise<Response>
-  }
+declare module "models" {
+    export type Puzzle = {
+        id: number;
+        print_date: string;
+        nyt_id: number | null;
+        difficulty: number | null;
+    };
+
+    export type Category = {
+        id: number;
+        puzzle_id: number;
+        difficulty: number;
+        category: string;
+        hint_card_id: number | null;
+    };
+
+    export type Card = {
+        id: number;
+        puzzle_id: number;
+        category_id: number;
+        position: number;
+        content: string;
+    };
 }
