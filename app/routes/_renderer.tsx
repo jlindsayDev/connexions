3eebadd0e5a449b1d26e108827033cd7167f7d23
Josh Lindsay <182923+jlindsayDev@users.noreply.github.com>
2025-06-01 11:36:43 -0700

    add styles to default renderer

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,8 +1,14 @@
+import { css, Style } from "hono/css";
+import { StrictMode } from "hono/jsx";
 import { jsxRenderer } from "hono/jsx-renderer";
 import { Link, Script } from "honox/server";
 
-export default jsxRenderer(({ children }) => {
-    return (
+const bodyClass = css`
+    background-color: rgb(229 231 235);
+`;
+
+export default jsxRenderer(({ children }) => (
+    <StrictMode>
         <html lang="en">
             <head>
                 <meta charset="UTF-8" />
@@ -11,9 +17,13 @@ export default jsxRenderer(({ children }) => {
                     content="width=device-width, initial-scale=1.0"
                 />
                 <Link href="/app/style.css" rel="stylesheet" />
+                <Style />
                 <Script src="/app/client.ts" />
+                {/* {title ? <title>{title}</title> : <></>} */}
             </head>
-            <body>{children}</body>
+            <body class={bodyClass}>
+                <main>{children}</main>
+            </body>
         </html>
-    );
-});
+    </StrictMode>
+));
