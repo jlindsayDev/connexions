ac3818a50e3f02c0cea348464a9550a4f364bd95
Josh Lindsay <182923+jlindsayDev@users.noreply.github.com>
2025-07-01 16:41:15 +0300

    other changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,72 +1,33 @@
 // Change the import to use your runtime specific build
 import build from "@hono/vite-build/bun";
 import devServer from "@hono/vite-dev-server";
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { type ConfigEnv, defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-    // https://github.com/honojs/examples/blob/main/hono-vite-jsx/vite.config.ts
-    if (mode === "client") {
-        return {
-            esbuild: {
-                jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
-            },
-            build: {
-                rollupOptions: {
-                    input: "./src/client.tsx",
-                    output: {
-                        // dir: "dist",
-                        entryFileNames: "client.js",
-                        minify: false,
-                    },
-                },
-            },
-        };
-    }
-
-    if (mode === "sw") {
-        return {
-            esbuild: {
-                jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
-            },
-            build: {
-                rollupOptions: {
-                    input: "./src/sw.ts",
-                    output: {
-                        entryFileNames: "public/sw.js",
-                        minify: false,
-                    },
-                },
-            },
-            plugins: [
-                VitePWA({
-                    mode: "development",
-                    minify: false,
-                    registerType: "autoUpdate",
-                    injectRegister: false,
-                    manifest: {
-                        name: "Connexions",
-                        short_name: "Connexions",
-                        start_url: "/sw",
-                        display: "standalone",
-                        background_color: "#FF0000",
-                    },
-                }),
-            ],
-        };
-    }
-
+export default defineConfig((_configEng: ConfigEnv) => {
     return {
+        esbuild: {
+            jsxImportSource: "hono/jsx/dom",
+        },
         server: {
             allowedHosts: ["musing_boyd.orb.local"],
         },
+        // build: {
+        //     rollupOptions: {
+        //         input: ["./client.tsx", "./sw.ts"],
+        //         output: {
+        //             // dir: "dist",
+        //             // entryFileNames: ["client.js", "sw.js"],
+        //             minify: false,
+        //         },
+        //     },
+        // },
         plugins: [
             build({
-                entry: "src/index.tsx",
+                entry: "index.tsx",
                 minify: false,
             }),
             devServer({
-                entry: "src/index.tsx",
+                entry: "index.tsx",
             }),
         ],
     };
