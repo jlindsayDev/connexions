4b514df0eca378799988e8fb6ffa96f1f345905d
Josh Lindsay <182923+jlindsayDev@users.noreply.github.com>
2026-08-20 09:15:11 -0700

    fetch puzzle from source in Cloudflare Function

--- functions/puzzles/[date].js
+++ functions/puzzles/[date].js
@@ -15,7 +15,11 @@ export const onRequestGet = async (context) => {
   const query = "SELECT * FROM puzzles WHERE print_date = ?";
   const puzzleResponse = await context.env.DB.prepare(query).bind(date).run();
 
-  const response = Response.json(puzzleResponse);
+  const response = Response.json(
+    puzzleResponse.results.length
+      ? puzzleResponse.results
+      : await fetchPuzzleFromSource(new Date(date)),
+  );
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
@@ -26,4 +30,14 @@ export const onRequestPost = async (context) => {
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
+};
+
+const fetchPuzzleFromSource = async (date) => {
+  const ENCODED_URL =
+    "aHR0cHM6Ly93d3cubnl0aW1lcy5jb20vc3ZjL2Nvbm5lY3Rpb25zL3YyLw==";
+  const dateStr = date.toISOString().slice(0, 10);
+  const url = `${fromBase64(ENCODED_URL)}${dateStr}.json`;
+  const response = await fetch(url);
+  console.info(`HTTP GET [${response.status}] ${url}`);
+  return await response.json();
 };
